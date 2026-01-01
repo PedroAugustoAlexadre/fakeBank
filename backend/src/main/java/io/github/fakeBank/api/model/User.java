8be64dfc7aef6c0e10a0b0e399cf6c076df0ca52
pedro augusto alexandre <pedroAugustoAlexandre@proton.me>
2026-01-01 15:27:32 -0300

    feat: add User entity

---
+++ backend/src/main/java/io/github/fakeBank/api/model/User.java
@@ -0,0 +1,42 @@
+package io.github.fakeBank.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(unique = true)
+    private String document;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = Instant.now();
+    }
+}
